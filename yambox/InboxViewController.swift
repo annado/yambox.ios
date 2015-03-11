265d277efead5073d4ca1236a5514df7ad31da4a
Anna Do <annado@gmail.com>
2015-03-11 10:00:51 -0700

    add YammerClient

--- yambox/InboxViewController.swift
+++ yambox/InboxViewController.swift
@@ -16,15 +16,25 @@ class InboxViewController: UIViewController {
         User.currentUser().logout()
     }
     
+    lazy var yammerClient : YammerClient = YammerClient()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         println("InboxViewController")
-        println(User.currentUser().userData)
+//        println(User.currentUser().userData)
         
         welcomeLabel.text = User.currentUser().name
+        
+        yammerClient.getInboxMessages({ (messages :AnyObject!) -> Void in
+            println("Messages: \(messages)")
+            }, failure: { (error: NSError!) -> Void in
+            println("Failed: \(error)")
+        })
     }
 
-    
+    func handleMessages(AnyObject!) {
+        
+    }
 
 }
--- yambox/YammerClient.swift
+++ yambox/YammerClient.swift
@@ -7,3 +7,23 @@
 //
 
 import Foundation
+
+class YammerClient {
+    
+    var httpClient : YMHTTPClient
+    
+    init() {
+        let url = NSURL(string: YAMMER_API_BASE_URL)
+        if (User.currentUser().isLoggedIn()) {
+            let token : NSString = YMLoginController.sharedInstance().storedAuthToken()
+            httpClient = YMHTTPClient(baseURL: url, authToken: token)
+        } else {
+            httpClient = YMHTTPClient(baseURL: url)
+        }
+    }
+    
+    func getInboxMessages(success: (AnyObject!) -> Void, failure: (NSError!) -> Void) {
+        var params : NSDictionary = ["limit": 30];
+        httpClient.getPath("/api/v1/messages/inbox.json", parameters: params, success: success, failure: failure)
+    }
+}
