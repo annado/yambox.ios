d02d2c3d24ece2a46645e9acbecf452b05adb8e0
Anna Do <annado@gmail.com>
2015-03-13 09:26:43 -0700

    add MarkAsRead to YammerClient

--- yambox/YammerClient.swift
+++ yambox/YammerClient.swift
@@ -26,13 +26,14 @@ class YammerClient {
         // filter=unarchived%3Binbox_unseen&threaded=extended&exclude_own_messages_from_unseen=true
         // older_than=10594043&last_seen_message_id=35190767
         
+        let path : String = "/api/v1/messages/inbox.json"
         var params : NSDictionary = ["limit": 10,
             "filter":"unarchived;inbox_unseen",
             "threaded":"extended",
             "exclude_own_messages_from_unseen":"true"
         ];
         
-        httpClient.getPath("/api/v1/messages/inbox.json", parameters: params, success: {(responseObject: AnyObject!) -> Void in
+        httpClient.getPath(path, parameters: params, success: {(responseObject: AnyObject!) -> Void in
             var messages : NSMutableArray = responseObject["messages"] as NSMutableArray
 //            messages.sortUsingComparator({ (a : AnyObject!, b : AnyObject!) -> NSComparisonResult in
 //                
@@ -41,4 +42,15 @@ class YammerClient {
             success(responseObject)
         }, failure: failure)
     }
+    
+    func markAsRead(threadId : NSString, messageId: NSString, success: (AnyObject!) -> Void, failure: (NSError!) -> Void) {
+        let path : String = "api/v1/messages/last_seen_in_thread.json"
+        
+        var params : NSDictionary = ["thread_id" : threadId,
+            "message_id" : messageId]
+        
+        httpClient.getPath(path, parameters: params, success: success, failure: failure)
+        
+    }
+    
 }
